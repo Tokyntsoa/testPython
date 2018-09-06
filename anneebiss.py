182024c441ba026f22ddb5869fa7f3755d192a44
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-09-06 13:52:44 +0300

    Update anneebiss.py

--- anneebiss.py
+++ anneebiss.py
@@ -2,15 +2,8 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def test():
-print("Test de programme en Python dans OpenShift qui calcul si une année est bissextile ou non.")
-annee = input ("Entrez une année : ")
-annee = int(annee)
-if(annee % 400 or (annee % 400 and annee % 100 != 0)) :
-	return "L année saisie est bissextile."
-else :
-	return "L année saisie n'est pas bissextile."
-#input('Appuyez sur ENTREE pour quitter')
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
